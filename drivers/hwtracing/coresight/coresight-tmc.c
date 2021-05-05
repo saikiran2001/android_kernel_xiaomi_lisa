f582c01acceec7394dcc1976773375c6de8a91c8
Arnd Bergmann <arnd@arndb.de>
2021-05-05 23:12:42 +0200

    Revert "test"
    This reverts commit 8ea835bef9cb5e7fd27c73e7ebd0bf0c0857a1ab.

--- drivers/hwtracing/coresight/coresight-tmc.c
+++ drivers/hwtracing/coresight/coresight-tmc.c
@@ -415,7 +415,7 @@ static ssize_t out_mode_store(struct device *dev,
 			      const char *buf, size_t size)
 {
 	struct tmc_drvdata *drvdata = dev_get_drvdata(dev->parent);
-	char str[11] = "";
+	char str[10] = "";
 	int ret;
 
 	if (strlen(buf) >= 10)
@@ -457,7 +457,7 @@ static ssize_t pcie_path_store(struct device *dev,
 			      const char *buf, size_t size)
 {
 	struct tmc_drvdata *drvdata = dev_get_drvdata(dev->parent);
-	char str[11] = "";
+	char str[10] = "";
 
 	if (strlen(buf) >= 10)
 		return -EINVAL;
