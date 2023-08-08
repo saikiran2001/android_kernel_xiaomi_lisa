StatiXOS/android_kernel_xiaomi_venus

scripts: gki: Add device-specific config fragments conditionally
Co-authored-by: ralph950412 <ralph950412@gmail.com>
Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

scripts/gki/envsetup.sh
@@ -23,6 +23,7 @@ PLATFORM_NAME=$1
 BASE_DEFCONFIG=${KERN_SRC}/arch/${ARCH}/configs/${2:-gki_defconfig}
 
 # Fragements that are available for the platform
+XIAOMI_DEVICE_QGKI_FRAG=${CONFIGS_DIR}/${DEVICE}_QGKI.config
 XIAOMI_GKI_FRAG=${CONFIGS_DIR}/xiaomi_GKI.config
 XIAOMI_QGKI_FRAG=${CONFIGS_DIR}/xiaomi_QGKI.config
 QCOM_GKI_FRAG=${CONFIGS_DIR}/${PLATFORM_NAME}_GKI.config
@@ -43,4 +44,4 @@ QCOM_GENERIC_PERF_FRAG=${CONFIGS_DIR}/${PLATFORM_NAME}.config
 QCOM_GENERIC_DEBUG_FRAG=${CONFIGS_DIR}/${PLATFORM_NAME}-debug.config
 
 export ARCH CROSS_COMPILE REAL_CC HOSTCC HOSTLD HOSTAR KERN_SRC KERN_OUT \
-	CONFIGS_DIR BASE_DEFCONFIG XIAOMI_GKI_FRAG XIAOMI_QGKI_FRAG QCOM_GKI_FRAG QCOM_QGKI_FRAG QCOM_DEBUG_FRAG
+	CONFIGS_DIR BASE_DEFCONFIG XIAOMI_DEVICE_QGKI_FRAG XIAOMI_GKI_FRAG XIAOMI_QGKI_FRAG QCOM_GKI_FRAG QCOM_QGKI_FRAG QCOM_DEBUG_FRAG

saikiran2001/android_kernel_xiaomi_lisa

scripts/gki/envsetup.sh
@@ -23,6 +23,7 @@ PLATFORM_NAME=$1
 BASE_DEFCONFIG=${KERN_SRC}/arch/${ARCH}/configs/${2:-gki_defconfig}
 
 # Fragements that are available for the platform
+XIAOMI_DEVICE_QGKI_FRAG=${CONFIGS_DIR}/${TARGET_PRODUCT}_QGKI.config
 XIAOMI_GKI_FRAG=${CONFIGS_DIR}/xiaomi_GKI.config
 XIAOMI_QGKI_FRAG=${CONFIGS_DIR}/xiaomi_QGKI.config
 QCOM_GKI_FRAG=${CONFIGS_DIR}/${PLATFORM_NAME}_GKI.config
@@ -43,4 +44,4 @@ QCOM_GENERIC_PERF_FRAG=${CONFIGS_DIR}/${PLATFORM_NAME}.config
 QCOM_GENERIC_DEBUG_FRAG=${CONFIGS_DIR}/${PLATFORM_NAME}-debug.config
 
 export ARCH CROSS_COMPILE REAL_CC HOSTCC HOSTLD HOSTAR KERN_SRC KERN_OUT \
-	CONFIGS_DIR BASE_DEFCONFIG XIAOMI_GKI_FRAG XIAOMI_QGKI_FRAG QCOM_GKI_FRAG QCOM_QGKI_FRAG QCOM_DEBUG_FRAG
+	CONFIGS_DIR BASE_DEFCONFIG XIAOMI_DEVICE_QGKI_FRAG XIAOMI_GKI_FRAG XIAOMI_QGKI_FRAG QCOM_GKI_FRAG QCOM_QGKI_FRAG QCOM_DEBUG_FRAG